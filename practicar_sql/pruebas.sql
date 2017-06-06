6e91966177bfe7b4b4399a0565edca04c9d73f03
David Artero <d.arterom@gmail.com>
2017-06-06 13:15:32 +0200

    ej4 - ej9:  procedures and functions

--- practicar_sql/pruebas.sql
+++ practicar_sql/pruebas.sql
@@ -1,5 +1,6 @@
 
 --Bloques
+--buscar empleados y departamentos
 set serveroutput on
 set verify off
 set echo off
@@ -9,6 +10,7 @@ nomEmp(&id_emp);
 Emp(&id_emp);
 end;
 
+--insetar departamento
 set serveroutput on
 set verify off
 set echo off
@@ -19,6 +21,58 @@ accept loc_dep prompt 'inserta la localizacion'
 begin
 dep_ins(&id_dep, '&nom_dep', &man_dep, &loc_dep);
 end;
+
+--borrar departamento
+set serveroutput on
+set verify off
+set echo off
+accept id_dep prompt 'inserta un codigo de departamento'
+begin
+dep_del(&id_dep);
+end;
+
+--contar empleados
+set serveroutput on
+set verify off
+set echo off
+accept id_dep prompt 'inserta un codigo de departamento'
+declare
+total number;
+begin
+total:=contar(&id_dep);
+dbms_output.put_line('El numero de empleados es de: '||total);
+end;
+
+--cambiar comision
+set serveroutput on
+set verify off
+set echo off
+accept emp_id prompt 'inserta el salario'
+declare
+salario employees.salary%type;
+comision employees.commission_pct%type;
+begin
+  select salary
+    into salario
+  from employees
+    where employee_id = &emp_id;
+
+  if salario<10000 then
+    comision:= 0.1;
+  elsif salario BETWEEN 10000 AND 15000 then
+    comision:= 0.15;
+  elsif salario >=20000 then
+    comision:= 0.20;
+  else
+    comision:= 0;
+  end if;
+  
+  update employees
+  set COMMISSION_PCT = comision
+    where employee_id = &emp_id;
+    dbms_output.put_line('La comision fue cambiada a: '||comision);
+end;
+
 
 --procedures
 --ej1
@@ -61,6 +115,15 @@ values(id_dep, nom_dep, man_dep, loc_dep);
 dbms_output.put_line('Department inserted');
 end;
 
+create or replace
+procedure dep_del(id_dep number)
+is
+begin
+delete from departments
+where department_id = id_dep;
+dbms_output.put_line('Department delete!!');
+end;
+
 --ej4
 create or replace
 procedure Emp(emp_id number)
@@ -78,3 +141,17 @@ where employee_id = emp_id;
 dbms_output.put_line(cod||'  '||nom||'  '||jobs||'  '||salario);
 dep(dept);
 end;
+
+--function 
+
+create or replace function contar(dept_id number)
+return number
+as
+cuenta number;
+begin
+  select count(employee_id)
+    into cuenta
+  from employees
+    where department_id=dept_id;
+  return cuenta;
+end contar;
